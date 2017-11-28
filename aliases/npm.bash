dc71370fe00decf0783894b7deeced14108686f0
Matthew Borgman <574527+matthewborgman@users.noreply.github.com>
2017-11-28 08:01:44 -0600

    Add `nrsl` alias to run Stylelint tests

--- aliases/npm.bash
+++ aliases/npm.bash
@@ -9,6 +9,7 @@ if commandExists npm; then
 	alias ni='npm install --loglevel error'	# ni:	Install packages
 	alias nl='npm link'						# nl:	Link packages
 	alias nrl='npm run lint'				# nrl:	Run lint tests
+	alias nrsl='npm run stylelint'			# nrsl:	Run stylelint tests
 	alias nrt='npm run test'				# nrt:	Run unit tests
 	alias ns='npm start'					# ns:	"Start"
 	alias nsd='npm run start:dev'			# nsd:	"Start" in development mode

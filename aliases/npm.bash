f0abf20930d1c84638348b0c76dd197e0c762fa9
Matthew Borgman <574527+matthewborgman@users.noreply.github.com>
2018-01-03 09:26:42 -0600

    Clarify comment for `nl` alias

--- aliases/npm.bash
+++ aliases/npm.bash
@@ -7,7 +7,7 @@ source "${DOTFILES_PATH}/functions/bootstrap.bash"
 if commandExists npm; then
 
 	alias ni='npm install --loglevel error'	# ni:	Install packages
-	alias nl='npm link'						# nl:	Link packages
+	alias nl='npm link'						# nl:	Add "global" link to package or link to other global package if specified
 	alias nrl='npm run lint'				# nrl:	Run lint tests
 	alias nrsl='npm run stylelint'			# nrsl:	Run stylelint tests
 	alias nrt='npm run test'				# nrt:	Run unit tests

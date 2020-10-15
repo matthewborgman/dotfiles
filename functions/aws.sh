1c4082b440665d1eac661c383d234a4de991f7e5
Matthew Borgman <574527+matthewborgman@users.noreply.github.com>
2020-10-15 09:22:51 -0500

    Update `awsauth` verify a profile is provided and set $AWS_PROFILE

--- functions/aws.sh
+++ functions/aws.sh
@@ -4,10 +4,17 @@
 
 ## Authenticate with AWS using the provided profile, via Okta
 awsauth() {
-    okta-awscli --okta-profile "$1" --profile "$1"
+    if [[ -z "$1" ]]; then
 
-    wait
-    echo "\n Successfully authenticated using profile \"$1\". Current identity:\n"
-    aws --profile "$1" sts get-caller-identity | bat --language json
-    echo "\n"
+        echo '`awsauth` requires the name of a Okta/AWS profile to use.'
+    else
+
+        okta-awscli --okta-profile "$1" --profile "$1"
+
+        wait
+        export AWS_PROFILE="$1"
+        echo "\n Successfully authenticated using profile \"$1\". Current identity:\n"
+        aws --profile "$1" sts get-caller-identity | bat --language json
+        echo "\n"
+    fi
 }

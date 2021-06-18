28c94206f6114bf7ca5286840ba43fe05b2edee0
Matthew Borgman <574527+matthewborgman@users.noreply.github.com>
2021-06-18 11:46:04 -0500

    Moved and renamed `rmnm` to `rmdnm`, cleaned up file formatting

--- aliases/npm.sh
+++ aliases/npm.sh
@@ -6,12 +6,13 @@ source "${DOTFILES_PATH}/functions/bootstrap.sh"
 
 if commandExists npm; then
 
-    alias ni='npm install --loglevel error'	# ni:	Install packages
-    alias nl='npm link'						# nl:	Add "global" link to package or link to other global package if specified
-    alias nrl='npm run lint'				# nrl:	Run lint tests
-    alias nrsl='npm run stylelint'			# nrsl:	Run stylelint tests
-    alias nrt='npm run test'				# nrt:	Run unit tests
-    alias nrtd='npm run test:debug'			# nrtd:	Run unit tests in debug mode
-    alias ns='npm start'					# ns:	"Start"
-    alias nsd='npm run start:dev'			# nsd:	"Start" in development mode
+    alias ni='npm install --loglevel error' # ni:    Install packages
+    alias nl='npm link'                     # nl:    Add "global" link to package or link to other global package if specified
+    alias nrl='npm run lint'                # nrl:   Run lint tests
+    alias nrsl='npm run stylelint'          # nrsl:  Run stylelint tests
+    alias nrt='npm run test'                # nrt:   Run unit tests
+    alias nrtd='npm run test:debug'         # nrtd:  Run unit tests in debug mode
+    alias ns='npm start'                    # ns:    "Start"
+    alias nsd='npm run start:dev'           # nsd:   "Start" in development mode
+    alias rmdnm='rm -fR node_modules'       # rmdnm: Remove "node_modules" directory
 fi
